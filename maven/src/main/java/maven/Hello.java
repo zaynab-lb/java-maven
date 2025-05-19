dbfc76fc39e5c75969fe8a269e79a49ebe34bdf3
zaynab-lb <zaynablboudaoudi@gmail.com>
2025-05-19 16:08:40 +0100

    Update Hello.java

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	
 	public static void main(String[] args)
     {
-        System.out.println("Hello DevOps & Cloud Computing !!!");
+        System.out.println("Hello DevOps & Cloud Computing !!! Zaynab Here !!!");
     }
 
 }
